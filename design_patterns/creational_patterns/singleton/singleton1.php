d5c335106cb5afd76cc3988618a213d310c042f0
ANkulagin <ankulskull@gmail.com>
2024-03-15 21:34:51 +0300

    реализован паттерн singleton

---
+++ design_patterns/creational_patterns/singleton/singleton1.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+class Singleton
+{
+    private static ?self $instance = null;
+
+    public static function getInstance(): self
+    {
+         return  self::$instance ??= new self();
+    }
+}
+
+$connected1 = Singleton::getInstance();
+$connected2 = Singleton::getInstance();
+var_dump($connected1 === $connected2);
